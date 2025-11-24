41379abf4aee6d0ed9ad80657b72a69cbfa5e378
AralGngr <aralgngr4@gmail.com>
2025-11-24 21:53:44 +0400

    Update nested-loops.js

--- Module-2/nested-loops.js
+++ Module-2/nested-loops.js
@@ -6,3 +6,10 @@ for (var i = 2023; i < 2025; i++) {
         console.log("--- Month: " + j);
     }
 }
+
+/*
+November 24 - Theory Note:
+Today we read about Nested Loops.
+Important: Nested loops are costly in performance (O(n^2)).
+They should be used with caution on large data sets.
+*/
